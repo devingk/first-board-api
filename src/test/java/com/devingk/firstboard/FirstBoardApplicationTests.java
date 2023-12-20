d8de4e951d0c4d7c64ed39bedbc6a98af8b2a90f
devingk <devcharmingk@naver.com>
2023-12-21 04:06:47 +0900

    initial commit

---
+++ src/test/java/com/devingk/firstboard/FirstBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devingk.firstboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
